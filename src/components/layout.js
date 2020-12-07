8237084c0fa88483a6863f24ed3d652fe0251d1d
Daniel <danieljust2395@gmail.com>
2020-12-06 23:31:34 -0600

    basic global styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,20 +1,13 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
+import { GlobalStyles } from "./styles/GlobalStyles"
 import "./layout.css"
 
 const Layout = ({ children }) => {
   return (
     <>
-      <div>
-        <main>{children}</main>
-      </div>
+      <GlobalStyles />
+      <main>{children}</main>
     </>
   )
 }
---
+++ src/components/styles/ColorStyles.js
@@ -0,0 +1,13 @@
+export const themes = {
+  dark: {
+    lightText: "#CCD6F6",
+    darkText: "#8892B0",
+    primary: "#64FFDA",
+    secondary: "#8BEAFF",
+    blueGradient:
+      "background: linear-gradient(180deg, #EDF8FF 0%, #3E9CD8 100%);",
+    greenGrandient:
+      "background: linear-gradient(180deg, #F3FAF8 0%, #51EDC8 100%);",
+    backgroundColor: `#0A192F`,
+  },
+}
