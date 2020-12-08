6bb7c7ab35f7a2ed942309f8938a4c1a4b532e03
Daniel <danieljust2395@gmail.com>
2020-12-07 22:41:56 -0600

    About me section

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -7,41 +7,40 @@ const About = () => {
     <Wrapper>
       <TitleWrapper>
         <Title>
-          <span>01.</span> About me
-          <hr />
+          <span>01.</span>
+          About Me
         </Title>
       </TitleWrapper>
 
       <ContentWrapper>
         <TextWrapper>
-          Hello! I'm Brittany, a software engineer based in Boston, MA.
-          <br />
-          <br />I enjoy creating things that live on the internet, whether that
-          be websites, applications, or anything in between. My goal is to
-          always build products that provide pixel-perfect, performant
-          experiences.
-          <br />
-          <br /> Shortly after graduating from Northeastern University, I joined
-          the engineering team at Upstatement where I work on a wide variety of
-          interesting and meaningful projects on a daily basis. <br />
-          <br />
-          Here are a few technologies I've been working with recently:
-          <br />
-          <br />
-          <TechWrapper>
-            <ul>
-              <li>Javascript</li>
-              <li>Javascript</li>
-              <li>Javascript</li>
-            </ul>
-            <ul>
-              <li>Javascript</li>
-              <li>Javascript</li>
-              <li>Javascript</li>
-            </ul>
-          </TechWrapper>
+          <Text>
+            Hello! I'm Brittany, a software engineer based in Boston, MA.
+          </Text>
+          <Text>
+            I enjoy creating things that live on the internet, whether that be
+            websites, applications, or anything in between. My goal is to always
+            build products that provide pixel-perfect, performant experiences.
+          </Text>
+          <Text>
+            Shortly after graduating from Northeastern University, I joined the
+            engineering team at Upstatement where I work on a wide variety of
+            interesting and meaningful projects on a daily basis.
+          </Text>
+          <Text>
+            Here are a few technologies I've been working with recently:
+          </Text>
+
+          <SkillsUl>
+            <SkillLi>Javascript</SkillLi>
+            <SkillLi>HTML & CSS</SkillLi>
+            <SkillLi>React</SkillLi>
+            <SkillLi>Angular 2+</SkillLi>
+            <SkillLi>Node.js</SkillLi>
+            <SkillLi>Django</SkillLi>
+          </SkillsUl>
         </TextWrapper>
-        <ImgWrapper></ImgWrapper>
+        <ImgWrapper />
       </ContentWrapper>
     </Wrapper>
   )
@@ -50,17 +49,31 @@ const About = () => {
 export default About
 
 const Wrapper = styled.div`
-  padding: 0 4rem;
-
-  border: 1px solid red;
+  margin: 0px auto;
+  padding: 100px 0px;
+  max-width: 900px;
 `
 
 const TitleWrapper = styled.div`
   display: flex;
-  border: 1px solid red;
+  align-items: center;
+  position: relative;
+  white-space: nowrap;
+  margin: 10px 0 40px;
 
   span {
     color: ${themes.dark.primary};
+  }
+
+  ::after {
+    content: "";
+    display: block;
+    position: relative;
+    top: -5px;
+    width: 300px;
+    height: 1px;
+    margin-left: 20px;
+    background-color: ${themes.dark.secondary};
   }
 `
 const Title = styled(H3)`
@@ -69,19 +82,36 @@ const Title = styled(H3)`
 
 const ContentWrapper = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr;
-  border: 1px solid red;
+  grid-template-columns: 3fr 2fr;
+  column-gap: 50px;
 `
 const TextWrapper = styled.div`
   line-height: 1.5;
   color: ${themes.dark.darkText};
-  border: 1px solid red;
+`
+const Text = styled.p`
+  margin: 0px 0px 15px;
+`
+const SkillsUl = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(2, minmax(140px, 200px));
+  padding: 0px;
+  margin: 20px 0px 0px;
+  overflow: hidden;
+  list-style: none;
+`
+const SkillLi = styled.li`
+  position: relative;
+  margin-bottom: 10px;
+  padding-left: 20px;
+
+  ::before {
+    content: "▹";
+    position: absolute;
+    left: 0px;
+    color: ${themes.dark.primary};
+    font-size: 13px;
+  }
 `
 
-const TechWrapper = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-`
-const ImgWrapper = styled.div`
-  border: 1px solid red;
-`
+const ImgWrapper = styled.div``
--- src/components/sections/Work.jsx
+++ src/components/sections/Work.jsx
@@ -1,7 +1,4 @@
 import React from "react"
-import styled from "styled-components"
-import { themes } from "../styles/ColorStyles"
-import { H3 } from "../styles/TextStyles"
 
 const Work = () => {
   return <div></div>
